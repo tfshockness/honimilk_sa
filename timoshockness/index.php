33b59eafe6683dd074ebc2288c0f39a2c4a95184
tfshockness <timoteo.ppt@gmail.com>
2017-01-20 01:11:55 -0800

    Set Structure and Menu Top
    Set the files structures and folders. I finished the top menu. Need add
    the JS effects.

---
+++ timoshockness/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Timoteo Shockness. Web Designer. Web Developer. Wordpress. SEO. Search Enginer Optimization. Creating website. Hosting. PHP. HTML. CSS. C#. ASP.NET. JavaScript" />
+        <meta name="og:type" content="website" />
+        <meta name="og:title" content="Timoteo Shockness, web designer and web developer. Technical skills in SEO, Wordpress, PHP, C#, ASP.NET, HTML, CSS, JavaScript." />
+        <meta name="og:url" content="http://www.timoshockness.com" />
+           <!--End-of-Meta-Tag-->
+           <link href="./css/styles.css" rel="stylesheet" type="text/css">
+        <title>Timoteo Shockness. Web Design and Web Developer. Personal Portfolio.</title>
+        
+        <style>
+ html{
+     margin: 0;
+     padding: 0;
+ }
+ a {
+     text-decoration: none;
+ }
+.navigation{
+    background:#fff;
+    width:100%;
+    position: absolute;
+    opacity: 1;
+    height: 53px;
+    top:0px;
+    left: 0px;
+    box-shadow:0 4px 2px 0px rgba(179,179,179, .5);
+    -webkit-box-shadow:0 4px 2px 0px rgba(179,179,179, .5);
+}
+
+.navigation a {
+    color:#535353;
+    display:block;
+    height: 34px;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+    -webkit-transition: color .1s;
+    transition: color .1s;
+}
+
+.navigation ul{ 
+    list-style:none;
+    text-decoration:none;
+ }
+ 
+.wrap-nav{
+    text-align:center;
+    margin: 0 auto;
+}
+
+.main_menu_list{
+    min-width: 750px;
+
+}
+.main_menu_list li{
+    float:left;
+    width: 15%;
+    height: 34px;
+    vertical-align:central;
+    line-height:80%;
+    padding-left:2%;
+    padding-right:2%;
+}
+
+#li_skills a:hover{
+    border-bottom: 3px solid #308080;
+    color: #308080;
+
+}
+
+#li_qualifications a:hover{
+    border-bottom: 3px solid #314980;
+    color: #314980;
+
+}
+
+#li_career a:hover{
+    border-bottom: 3px solid #138BC4;
+    color: #138BC4;
+
+}
+
+#li_portifolio a:hover{
+    border-bottom: 3px solid #F0B14E;
+    color: #F0B14E;
+
+}
+
+#li_contact a:hover{
+    border-bottom: 3px solid #CAC3AB;
+    color: #CAC3AB;
+
+}
+
+        </style>
+    </head>
+    <body>
+        <?php include_once 'resources/templates/menu_top.php'; ?>
+        <div id="content">
+            
+        </div>
+        <!-- Scripts -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    </body>
+</html>
