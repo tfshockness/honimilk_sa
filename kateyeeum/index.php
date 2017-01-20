de5790b67fa777361696006462cffaacf188a9e8
tfshockness <timoteo.ppt@gmail.com>
2017-01-20 01:28:31 -0800

    Adding Files to Honi and Folder
    Just to add a Honi Project Folder with Sample files.

---
+++ kateyeeum/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Timoteo Shockness. Web Designer. Web Developer. Wordpress. SEO. Search Enginer Optimization. Creating website. Hosting. PHP. HTML. CSS. C#. ASP.NET. JavaScript" />
+    <meta name="og:type" content="website" />
+    <meta name="og:title" content="Timoteo Shockness, web designer and web developer. Technical skills in SEO, Wordpress, PHP, C#, ASP.NET, HTML, CSS, JavaScript." />
+    <meta name="og:url" content="http://www.timoshockness.com" />
+     <!--End-of-Meta-Tag-->
+    <title>Timoteo Shockness. Web Design and Web Developer. Personal Portfolio.</title>
+    <!--Scripts-and-Css-->
+    <link rel="stylesheet" type="text/css" href="./css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="./css/my.css" />
+    <link rel="stylesheet" type="text/css" href="./css/m-my.css" />
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400i|Ubuntu" rel="stylesheet"> 
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="./js/typed.js" type="text/javascript"></script>
+</head>
+<body>
+  <div id="app"></div>
+
+</body>
+</html>
