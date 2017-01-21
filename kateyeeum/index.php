f0a4ec5fd439186a1c69bc38e0453113d6bf5c6b
yiyeum <KateYeEumLee@iyeeums-air.wifi.bcit.ca>
2017-01-21 12:01:07 -0800

    work for main board (1)
    added background color, modified the fonts for h1 and a tags

--- kateyeeum/index.php
+++ kateyeeum/index.php
@@ -4,24 +4,38 @@
   <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Timoteo Shockness. Web Designer. Web Developer. Wordpress. SEO. Search Enginer Optimization. Creating website. Hosting. PHP. HTML. CSS. C#. ASP.NET. JavaScript" />
+    <meta name="description" content="Kate Yeeum. Web Designer. Web Developer. Wordpress. SEO. Search Enginer Optimization. Creating website. Hosting. PHP. HTML. CSS. JavaScript" />
     <meta name="og:type" content="website" />
-    <meta name="og:title" content="Timoteo Shockness, web designer and web developer. Technical skills in SEO, Wordpress, PHP, C#, ASP.NET, HTML, CSS, JavaScript." />
-    <meta name="og:url" content="http://www.timoshockness.com" />
+    <meta name="og:title" content="Kate Yeeum. Technical skills in SEO, Wordpress, PHP, HTML, CSS, JavaScript." />
+    <meta name="og:url" content="http://www.kateyeeum.com" />
      <!--End-of-Meta-Tag-->
-    <title>Timoteo Shockness. Web Design and Web Developer. Personal Portfolio.</title>
+    <title>Kate Yeeum - Web Designer</title>
     <!--Scripts-and-Css-->
-    <link rel="stylesheet" type="text/css" href="./css/reset.css" />
-    <link rel="stylesheet" type="text/css" href="./css/my.css" />
-    <link rel="stylesheet" type="text/css" href="./css/m-my.css" />
-    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400i|Ubuntu" rel="stylesheet"> 
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro|Source+Sans+Pro:300" rel="stylesheet">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script src="./js/typed.js" type="text/javascript"></script>
 </head>
 <body>
-  <div id="app"></div>
+    <div class="main_board">
+        <div class="main_board_content">
+            <h1> Hi, </h1>
+            <h1> I am Kate </h1>
+            <h1> - Web Designer </h1>
+        </div> <!-- End of .main_board_content -->
+        <div class="main_board_links">
+            <span>
+                <i class="fa fa-file-pdf-o" id="pdf_icon" aria-hidden="true"></i>
+                <a href="#"> Download CV </a>
+            </span>
+            <span>
+                <i class="fa fa-linkedin" id="linkedIn_icon" aria-hidden="true"></i>
+                <a href="#"> LinkedIn </a>
+            </span>
+        </div> <!-- End of .main_board_links -->
+    </div> <!-- End of .main_board -->
 
 </body>
 </html>
