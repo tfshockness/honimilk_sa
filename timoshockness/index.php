d1dcb08f7b2f8986cfc0fad95755b16b0eb65f92
tfshockness <timoteo.ppt@gmail.com>
2017-01-31 18:56:05 -0800

    Icon added
    Changes in Index and Added folder with Icons

--- timoshockness/index.php
+++ timoshockness/index.php
@@ -13,8 +13,19 @@ and open the template in the editor.
         <meta name="og:type" content="website" />
         <meta name="og:title" content="Timoteo Shockness, web designer and web developer. Technical skills in SEO, Wordpress, PHP, C#, ASP.NET, HTML, CSS, JavaScript." />
         <meta name="og:url" content="http://www.timoshockness.com" />
+        
            <!--End-of-Meta-Tag-->
         <title>Timoteo Shockness. Web Design and Web Developer. Personal Portfolio.</title>
+        <link rel="shortcut icon" href="icon/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="icon/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="icon/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="icon/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="icon/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="icon/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="icon/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="icon/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="icon/apple-touch-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="icon/apple-touch-icon-180x180.png" />
         <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Orbitron|Roboto" rel="stylesheet">
         <link href="css/styles.css" rel="stylesheet" type="text/css">
         <script src="https://use.fontawesome.com/185e0bd75a.js"></script>
