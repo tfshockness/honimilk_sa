18817809437cdf6fea80eff645ab5760eb848dd9
tfshockness <timoteo.ppt@gmail.com>
2017-02-02 11:41:27 -0800

    Portifolio changes
    Added under construction msg and img on portfolio section.

--- timoshockness/resources/sections/portifolio.php
+++ timoshockness/resources/sections/portifolio.php
@@ -1,3 +1,8 @@
 <div>
     <h2 class="f_sans title portifolio_title">Some of My Work</h2>
+    <div class="maintenance">
+        <img src="img/layout/icons/maintenance.png" alt="Maintenance Icon" />
+        <h3 class="f_roboto">Under Construction</h3>
+        
+    </div>
 </div>
