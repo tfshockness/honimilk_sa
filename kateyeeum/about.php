4ba3a00493db66751fcf45ca2b86eb20edc63fe8
yiyeum <KateYeEumLee@iyeeums-air.wifi.bcit.ca>
2017-02-07 12:09:56 -0800

    about URL added

--- kateyeeum/about.php
+++ kateyeeum/about.php
@@ -26,11 +26,11 @@
         <div><h2>HIRE ME</h2></div>
     </div> <!-- End of .hire_me -->
     <div class="hire_contact">
-        <a id="contact_email" href="yeeumm@gmail.com"> yeeumm@gmail.com </a> <br />
+        <a id="contact_email" href="mailto:hello@kateyeeum.com"> hello@kateyeeum.com </a> <br />
         <div class="contact_social">
-            <a class="social_link" href="#"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
-            <a class="social_link" href=""><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-            <a class="social_link" href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
+            <a class="social_link" href="https://github.com/yiyeum" target="_blacnk"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+            <a class="social_link" href="https://twitter.com/KateYeeum" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+            <a class="social_link" href="https://www.facebook.com/yiyeum" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
         </div> <!-- End of .contact_social -->
     </div> <!-- End of .hire_contact -->
     
