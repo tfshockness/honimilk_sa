8e78da5fd1b30cec3f6c7e56cbb2c7c1d8b85f85
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-02-27 13:26:48 -0800

    Worked for responsive mobile menu
    responsive mobile menu - media query

--- kateyeeum/index.php
+++ kateyeeum/index.php
@@ -4,6 +4,7 @@
  <?php include_once 'header.php'; ?>
 </head>
 <body>
+  <div class="wrapper">
     <div class="main_board">
         <div class="main_board_content">
                 <a href="index.php">
@@ -24,8 +25,8 @@
                         <i class="fa fa-linkedin fa-stack-1x" id="linkedIn_icon" aria-hidden="true"></i>
                     </span>
                 </div>        
-            </div> <!-- End of .main_board_content -->
-        </div> <!-- End of .main_board_links -->
+            </div> <!-- End of .main_board_links -->
+        </div> <!-- End of .main_board_content -->
             <div class="display_title">
                 <a href="#" id="project_web">WEB DEVELOPMENT</a>
                 <a href="#" id="project_info">INFOGRAPHIC</a>
@@ -41,11 +42,20 @@
             <p class="main_blurb"> UX / SEO / Responsive </p>
             <p class="main_blurb"> Photoshop / Illustrator </p>
         </div> <!-- End of #content -->
-    </div> <!-- display -->
-    
+    </div> <!-- End of .display -->
+</div>
+      <div class="mobile">
+        <ul>
+          <li><a href="#web"><i class="fa fa-th-large fa-3x" aria-hidden="true"></i><br />WEB</a></li>
+          <li><a href="#info"><i class="fa fa-cube fa-3x" aria-hidden="true"></i><br />INFOGRAPHIC</a></li>
+          <li><a href="#about"><i class="fa fa-envelope fa-3x" aria-hidden="true"></i><br />ABOUT</a></li>
+        </ul>
+    </div><!--End of .mobile | mobile menu bar-->
+
 <!--    <footer>
         <p>Copyright © 2017 KATE YEEUM. All Rights Reserved.</p>
     </footer>-->
     <script src="js/script.js" type="text/javascript"></script>
 </body>
 </html>
+
