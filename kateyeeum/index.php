a7d5ad38e46421a3a862feb8ad90c9ea4fcc2a5a
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-08 21:38:09 -0700

    navigation part & main part working on

--- kateyeeum/index.php
+++ kateyeeum/index.php
@@ -5,46 +5,44 @@
 </head>
 <body>
   <div class="wrapper">
-    <div class="main_board">
-        <div class="main_board_content">
-                <a href="index.php">
-                    <h1> KATE YEEUM </h1>
-                    <h1> / </h1>
-                    <h1> WEB DESIGNER </h1>
-                </a>
+      <nav class="navbar navbar-expand-lg">
+          <div class="container">
+              <a class="navbar-brand" href="#">
+                  <img src="./image/signature.png" width="120" class="d-inline-block align-top" alt="signature">
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <ul class="navbar-nav ml-auto">
+                      <li class="nav-item active">
+                          <a class="nav-link" href="#">Work</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">About</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Contact</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
 
-            <div class="main_board_links">
-                <div>
-                    <span class="fa-stack fa-lg">
-                        <a href="image/kate_cv.pdf" download><i class="fa fa-file-pdf-o fa-stack-1x" id="pdf_icon" aria-hidden="true"></i></a>
-                    </span>
-                </div>
-
-                <div>
-                    <span class="fa-stack fa-lg">
-                        <a href="https://www.linkedin.com/in/kate-yeeum-lee-b3a971139/" target="_blank"><i class="fa fa-linkedin fa-stack-1x" id="linkedIn_icon" aria-hidden="true"></i></a>
-                    </span>
-                </div>        
-            </div> <!-- End of .main_board_links -->
-        </div> <!-- End of .main_board_content -->
-    </div> <!-- End of .main_board -->
-
-        <div id="content">
-            <div class="display_all_projects">
-                    <?php include_once 'db.php';
-                        $query = "SELECT * FROM webProject";
-                        $result = mysqli_query($dbconnection,$query);
-                        if(!$result){
-                            "database query failed";
-                        }
-                    ?>
-                <?php while($webProject = mysqli_fetch_object($result)){ ?>
-                        <div class="each_project" onClick="openDetailWeb('<?php echo $webProject->id ?>')">
-                                    <img src="image/web_cover/<?php echo $webProject->cover_img ?>" >
-                        </div> <!-- End of .each_project -->
-                <?php } ?>
-            </div> <!-- End of .display_all_projects -->
-        </div> <!-- End of #content -->
+      <div class="row">
+              <?php include_once 'db.php';
+                  $query = "SELECT * FROM webProject";
+                  $result = mysqli_query($dbconnection,$query);
+                  if(!$result){
+                      "database query failed";
+                  }
+              ?>
+              <?php while($webProject = mysqli_fetch_object($result)){ ?>
+                  <div class="col-md-4 each_project" onClick="openDetailWeb('<?php echo $webProject->id ?>')">
+                      <img src="image/web_cover/<?php echo $webProject->cover_img ?>" >
+                  </div> <!-- End of .each_project -->
+              <?php } ?>
+      </div><!--.row-->
 </div> <!-- End of .wrapper -->
     <footer>
         <p>Copyright © 2017 KATE YEEUM. All Rights Reserved.</p>
